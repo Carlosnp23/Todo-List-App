3ea3831ef3f0c05fd4db7f82088924d88bc5ad44
Carlos <cnoramb1@my.centennialcollege.ca>
2022-12-03 21:15:49 -0500

    Update commit

--- Todo List App/Note.swift
+++ Todo List App/Note.swift
@@ -4,6 +4,12 @@
 //
 //  Created by Carlos Norambuena on 2022-11-29.
 //
+//  File name: Todo List App
+//  Author's name: Carlos Norambuena Perez
+//  Student ID: 301265667
+//  Date: 2022-12-03
+//  App Description: Todo List App (Assignment 5)
+//  Version of Xcode: Version 14.1 (14B47b)
 
 import CoreData
 
